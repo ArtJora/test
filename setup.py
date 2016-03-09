3e819d78715815dfc02a87ea270dac0ac34ac291
ArtJora <flash.kramatorsk@gmail.com>
2016-03-09 17:59:49 +0200

    Initialization

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='testtest',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Markov',
+      author_email='alt00x8fq@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
      )
